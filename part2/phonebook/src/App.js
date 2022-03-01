e21c2c25e289fe797db013ff4b85fe56042d55a5
kostiya <kostiya@users.noreply.github.com>
2022-03-01 17:34:54 +0200

    Finished 2.10 - refactoring Phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,20 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
+const Filter = ({filter, setFilter}) => {
+  const handleFilterChange = (event) => setFilter(event.target.value)
+  return(
+  <div>
+      filter shown with: <input 
+                            value={filter}
+                            onChange={handleFilterChange}/>
 
+  </div>
+  )
+}
+
+const AddPersonForm = ({newName,setNewName,newNumber,setNewNumber,persons,setPersons}) => {
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
-  const handleFilterChange = (event) => setFilter(event.target.value)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -34,42 +35,63 @@ const App = () => {
     setNewNumber('')
   }
 
+  return(
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input 
+                value={newName}
+                onChange={handleNameChange}/>
+        <br />
+        number: <input 
+                value={newNumber}
+                onChange={handleNumberChange}/>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const NumbersTable = ({persons,filter}) => (
+  <table>
+    <tbody>
+      {persons
+      .filter(person => person.name.toLowerCase()
+      .includes(filter.toLowerCase()))
+      .map((person) =>  
+        <tr key={person.name}>
+          <td>{person.name}</td>
+          <td>{person.number}</td>
+        </tr>)}
+    </tbody>
+  </table>
+)
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-      filter shown with: <input 
-                  value={filter}
-                  onChange={handleFilterChange}/>
-
-      </div>
+      <Filter filter={filter} setFilter={setFilter} />
       <h2>Add new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input 
-                  value={newName}
-                  onChange={handleNameChange}/>
-          <br />
-          number: <input 
-                  value={newNumber}
-                  onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <AddPersonForm newName={newName} 
+                    setNewName={setNewName}
+                    newNumber={newNumber}
+                    setNewNumber={setNewNumber}
+                    persons={persons}
+                    setPersons={setPersons} />
       <h2>Numbers</h2>
-      <table>
-        <tbody>
-          {persons
-          .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
-          .map((person) =>  
-            <tr key={person.name}>
-              <td>{person.name}</td>
-              <td>{person.number}</td>
-            </tr>)}
-        </tbody>
-      </table>
+      <NumbersTable persons={persons} filter={filter} />
     </div>
   )
 }
