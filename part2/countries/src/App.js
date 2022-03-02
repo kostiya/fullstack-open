59c476e12aa99cc2418de999faca5771dca69a8b
kostiya <kostiya@users.noreply.github.com>
2022-03-02 18:38:56 +0200

    Finished 2.12

---
+++ part2/countries/src/App.js
@@ -0,0 +1,75 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const FindCountries = ({filter, setFilter}) => {
+  return(
+    <>
+      find countries:
+      <input value = {filter}
+            onChange={(event) => setFilter(event.target.value)} />
+    </>
+  )
+}
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+  useEffect(() =>{
+    axios.get('https://restcountries.com/v3.1/all')
+         .then(respons => setCountries(respons.data))
+  }, [])
+
+  if(countries.length === 0){
+    return(<h1>Loading countries</h1>)
+  }
+  
+  const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
+  
+  if(filteredCountries.length > 10){
+    console.log('Over 10', filteredCountries)
+    return(
+    <>
+    <FindCountries filter={filter} setFilter={setFilter} />
+    <h1>Too many countries, specify another filter.</h1>
+    </>)
+  }
+
+  if(filteredCountries.length > 1){
+    return(
+    <>
+      <FindCountries filter={filter} setFilter={setFilter} />
+      <table>
+        <tbody>
+          {filteredCountries.map((country) => <tr><td key={country.name.common}>{country.name.common}</td></tr>)}
+        </tbody>
+      </table>
+    </>
+    )
+  }
+
+  if(filteredCountries.length === 0){
+    return(
+    <>
+      <FindCountries filter={filter} setFilter={setFilter} />
+    </>
+    )
+  }
+
+  const country = filteredCountries[0]
+
+  return(
+  <>
+  <FindCountries filter={filter} setFilter={setFilter} />
+  <h1>{country.name.common}</h1>
+  capital {country.capital[0]} <br/>
+  area {country.area}
+  <h3>languages</h3>
+  <ul>
+    {Object.keys(country.languages).map(key => <li key={key}>{country.languages[key]}</li> )}
+  </ul>
+  <div style={{ fontSize : '150px'}}>{country.flag}</div>
+  </>)
+}
+
+export default App;
