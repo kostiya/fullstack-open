b8a104284f1d1784772e814aa72544cdec0a463a
kostiya <kostiya@users.noreply.github.com>
2022-05-22 22:20:28 +0300

    4.2 working as expected

---
+++ part4/blog-list-backend/app.js
@@ -0,0 +1,16 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('Connecting to MongoDB')
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
