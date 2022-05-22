958c63b62886072bc05b35b7fadb103981aa2913
kostiya <kostiya@users.noreply.github.com>
2022-05-22 23:56:12 +0300

    FInished 4.3-4.4

---
+++ part4/blog-list-backend/utils/list_helper.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    return blogs.map(blog => blog.likes).reduce((sum, value) => sum + value, 0)
+}
+
+module.exports = {
+    dummy,
+    totalLikes
+}
